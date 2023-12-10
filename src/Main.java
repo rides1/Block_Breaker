4783bfce083f6008ad3b8e4a2bfecd50850e5eba
rides1 <rpd35@njit.edu>
2023-12-10 00:03:24 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Block Breaker");
+
+        BlockPanel panel = new BlockPanel();
+        frame.getContentPane().add(panel);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+        frame.setSize(500,600);
+
+        //frame.setResizable(false);
+    }
+}
